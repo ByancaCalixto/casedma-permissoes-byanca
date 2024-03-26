5600dee8a4ceeefe6729f5714d47fa49b9796ea7
Byanca Calixto <byanca.calixtosouza@nttdata.com>
2024-03-26 12:11:47 -0300

    atividade esteira aws

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "control _account" {
+    type = string
+}
+variable "producer_role_arn_mesh" {
+    type = string
+}
+variable "database_sor" {
+    type = string
+}
+variable "database_sot" {
+    type = string
+}
+variable "database_spec" {
+    type = string
+}
